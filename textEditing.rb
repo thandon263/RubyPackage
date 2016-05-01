4aa75267fe4d9eee074cfbc081fd3b560900fc26
thandoN <ncubethando20@gmail.com>
2016-05-01 10:36:30 -0400

    Update textEditing.rb

--- textEditing.rb
+++ textEditing.rb
@@ -1,16 +1,16 @@
 (1..100).each do  |n|
-  i_3=(n%3==0)
-  i_5=(n%5==0)
+  i_3=(n%3==0) #Declare the variable int 3
+  i_5=(n%5==0) #Declare the variable int 5
   case
-      when i_3&&i_5
+      when i_3&&i_5 # for both print
         puts 'BitmakerHQ'
-      when i_3
+      when i_3 # for int 3 print 'Bitmaker'
         puts 'Bitmaker'
-      when i_5
+      when i_5 # for int 5 print 'HQ'
         puts 'HQ'
-      else
+      else # print all other numbers that are not multiples
         puts n
 
-  end
+  end #end case
 
-end  
+end  #end //
